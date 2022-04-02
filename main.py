d54f6916c0e56bddd3957b12249ceba8bab7c63b
jinwkim <jinwkim18@gmail.com>
2022-04-02 18:41:32 -0400

    Output predicted emotion on screen

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ camera = cv2.VideoCapture(0)
 # loop over the frames from the video stream while camera is active
 # fps = 30
 counter = 0
+detected_emotion = "None"
 while camera.isOpened():
 	# current frame - numpy.ndarray
 	s, frame = camera.read()
@@ -23,7 +24,10 @@ while camera.isOpened():
 	# if no input from camera, skip to next iteration
 	if not s:
 		continue
-
+	
+	# cv2.putText(image, text, org, font, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
+	cv2.putText(frame, detected_emotion, (20,30),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 1)
 	# show the output image with the face detections + facial landmarks
 	cv2.imshow("Output", frame)
 
@@ -34,8 +38,8 @@ while camera.isOpened():
 	# dim = (width, height)
 	# # resize image using INTER_AREA for interpolation
 	# frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
-	frame = cv2.resize(frame, (48,48))
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	resized = cv2.resize(frame, (48,48))
+	gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 
 	if counter % 60 == 0: # 30 fps
 		img = gray
@@ -43,6 +47,7 @@ while camera.isOpened():
 		x = np.expand_dims(x, axis = 0)
 		x /= 255
 		pred_vals = model.predict(x)[0] # [[0.09034569, 0.04079238, 0.13130878, 0.06450415, 0.44670576, 0.08373094, 0.14261228]]
+		detected_emotion = emotions[np.argmax(pred_vals)]
 		# s = "'angry' {0}, 'disgust' {1}, 'fear' {2}, 'happy' {3}, 'sad' {4}, 'surprise' {5}, 'neutral' {6}"
 		# print(s.format(*pred_vals))
 		print("Detected emotion: ", emotions[np.argmax(pred_vals)])
